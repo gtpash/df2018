b582cfab14b688767b0e7b67eeb06871e7e4c9b1
Vinit Ranjan <Vinit@Vinits-MacBook-Pro.local>
2018-04-06 22:57:20 -0400

    asdf

--- Vinit.R
+++ Vinit.R
@@ -1,6 +1,6 @@
 vinitWorkingDirectory <- "~/Documents/df2018"
 setwd(vinitWorkingDirectory)
-Packages <- c("dplyr", "ggplot2", "readr","magrittr")
+Packages <- c("dplyr", "ggplot2", "readr","magrittr","ggmap")
 lapply(Packages, library, character.only = TRUE)
 
 data <- "./data/datafest2018.csv"
@@ -9,3 +9,9 @@ datafest2018 <- read_csv(data)
 datafest2018 %>% group_by(companyId) %>% summarize(n()) -> companies
 datafest2018 %>% filter(companyId == "company00090") %>% mean(.$estimatedSalary)
 city_counts <- datafest2018 %>% group_by(city) %>% summarize(n()) %>% arrange(desc(`n()`)) %>% slice(2:12501)
+#write_csv(city_counts,"./data/city_counts.csv")
+industries <- datafest2018 %>% group_by(industry) %>% summarize(n()) %>% arrange(desc(`n()`))
+#n <- 1
+#locs <- city_counts$city[n:(n+2)]
+#
+#geoLocations <- geocode(as.character(locs))

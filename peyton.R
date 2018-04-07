1ebb2f4e424c1b1392abe73e04017d4675ac3a61
ppwood <ppwood@ncsu.edu>
2018-04-06 23:21:50 -0400

    My Basic File
    Good Luck to us

---
+++ peyton.R
@@ -0,0 +1,29 @@
+##DATAFEST
+##April 6-8, 2018
+##Peyton Wood, Jason Thompson, Graham Pash, Vinett, Sam
+
+
+##Set WD
+setwd("C:/Users/peyto/OneDrive/Documents/Datafest/df2018-master")
+Packages <- c("dplyr", "ggplot2", "readr","magrittr","ggmap")
+lapply(Packages, library, character.only = TRUE)
+
+data <- "./data/datafest2018.csv"
+datafest2018 <- read_csv(data)
+
+
+
+
+
+
+
+#Vinit's Code
+datafest2018 %>% group_by(companyId) %>% summarize(n()) -> companies
+datafest2018 %>% filter(companyId == "company00090") %>% mean(.$estimatedSalary)
+city_counts <- datafest2018 %>% group_by(city) %>% summarize(n()) %>% arrange(desc(`n()`)) %>% slice(2:12501)
+#write_csv(city_counts,"./data/city_counts.csv")
+industries <- datafest2018 %>% group_by(industry) %>% summarize(n()) %>% arrange(desc(`n()`))
+#n <- 1
+#locs <- city_counts$city[n:(n+2)]
+#
+#geoLocations <- geocode(as.character(locs))

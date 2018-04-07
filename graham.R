29029a77dd388d354afc036fc2e55729b9c24b3c
root <root@graham-pc.localdomain>
2018-04-07 15:41:24 -0400

    zerofillyboi

--- graham.R
+++ graham.R
@@ -1,21 +1,47 @@
 #setwd("C:/Users/graha/Dropbox/Graham/documents/GitHub/df2018")
-# mykey <- "AIzaSyBz0PcAiiaSE_PGKXivzUGOk8vw4fs40Ek" #graham
+# mykey <- "AIzaSyDbTwdfJGvyiPpNJ_VaLj2TvLvimcV7AUQ" #graham
 
-packs <- c("dplyr","ggplot2","readr","magrittr","ggmap")
+packs <- c("dplyr","ggplot2","readr","magrittr","ggmap","lubridate")
 lapply(packs,library,character.only = TRUE)
 
-raw <- readr::read_csv("./datafest2018.csv")
+raw <- readr::read_csv("./data/datafest2018.csv")
 
 company <- summarise(group_by(raw,companyId), count = n())
 
 # save city locations in csv
 city_counts <- raw %>% group_by(city) %>% summarize(n()) %>% arrange(desc(`n()`)) %>% slice(2:12501)
 
-n <- 1
-locs <- city_counts$city[n:(n+20)]
+# zero-fill click data
+samp <- sample_n(raw,1000)
+daterange_jobId <- raw %>% group_by(jobId) %>% filter(max(jobAgeDays) > 0) %>% summarize(min(date),max(jobAgeDays))
+hist(daterange_jobId$postlength)
 
-ggmap::register_google(key = mykey)
+# zero-fill a time series for specific jobId
+zerofillyboi <- function(job_id, raw){
+  
+  incomplete <- raw %>% filter(jobId == job_id) %>% arrange(date)
+  
+  daterange <- incomplete %>% summarize(min(date),as.double(difftime(max(date),min(date),units="days")))
+  colnames(daterange) <- c("firstdate","postlength")
+  fulldates <- data.frame(date = seq(from = daterange$firstdate, by = "day", length.out = daterange$postlength+1))
+  
+  zerofill <- merge(fulldates,incomplete,by = "date",all.x = TRUE)
+  
+  update <- c("clicks","localClicks") 
+  zerofill[update][is.na(zerofill[update])] <- 0
+  
+  invariant <- zerofill %>% select(companyId:educationRequirement) %>% colnames()
+  zerofill[invariant] <- zerofill %>% slice(1) %>% select(invariant) 
+  
+  zerofillyboi <- zerofill
+  
+}
 
-locs <- cbind(ggmap::geocode(as.character(locs)),locs)
-raw <- cbind(raw,geoLocations)
-
+# timing test
+testjobs <- sample_n(raw,10)
+time1<-proc.time()
+timeseriesjobs <- NULL
+for(i in 1:length(testjobs)){
+  timeseriesjobs <- rbind(timeseriesjobs,zerofillyboi(testjobs$jobId[i],raw))
+}
+print(paste0(round(((proc.time() - time1)[3])/60,digits = 3)," minutes from start"))

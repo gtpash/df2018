7569882d21b341f34df1c6c7df129b4567b3ba73
Vinit Ranjan <Vinit@Vinits-MacBook-Pro.local>
2018-04-07 14:44:44 -0400

    aggregate_health

--- StateAggregateUnique.R
+++ StateAggregateUnique.R
@@ -7,5 +7,10 @@ data <- "./data/datafest2018.csv"
 datafest2018 <- read_csv(data)
 
 samp <- datafest2018 %>% sample_n(1000)
-unique <- datafest2018 %>% distinct(jobId, .keep_all = TRUE) %>% filter(country == 'US') %>% group_by(stateProvince) %>% summarize(ncity = n())
+unique <- samp %>% distinct(jobId, .keep_all = TRUE) %>% filter(country == 'US') %>% group_by(stateProvince) %>% summarize(ncity = n())
+unique_health <- datafest2018 %>% distinct(jobId, .keep_all = TRUE) %>% filter(country == 'US', industry == 'HEALTH_CARE') %>% group_by(stateProvince) %>% summarize(ncity = n())
+
+
+#unique <- datafest2018 %>% distinct(jobId, .keep_all = TRUE) %>% filter(country == 'US') %>% group_by(stateProvince) %>% summarize(ncity = n())
 #write_csv(unique,"./data/state_aggregate.csv")
+#write_csv(unique_health, "./data/state_aggregate_health.csv")

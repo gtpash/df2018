3580e4c5276796683a5159b0c27306a62841f43f
Vinit Ranjan <Vinit@Vinits-MacBook-Pro.local>
2018-04-06 22:12:57 -0400

    city counts

--- Vinit.R
+++ Vinit.R
@@ -7,3 +7,5 @@ data <- "./data/datafest2018.csv"
 datafest2018 <- read_csv(data)
 
 datafest2018 %>% group_by(companyId) %>% summarize(n()) -> companies
+datafest2018 %>% filter(companyId == "company00090") %>% mean(.$estimatedSalary)
+city_counts <- datafest2018 %>% group_by(city) %>% summarize(n()) %>% arrange(desc(`n()`)) %>% slice(2:12501)

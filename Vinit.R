24bad18d76e3a47fc38eee13124f5ed44da9f712
Vinit Ranjan <Vinit@Vinits-MacBook-Pro.local>
2018-04-07 13:57:38 -0400

    tedst

--- Vinit.R
+++ Vinit.R
@@ -7,7 +7,7 @@ data <- "./data/datafest2018.csv"
 datafest2018 <- read_csv(data)
 
 datafest2018 %>% group_by(companyId) %>% summarize(n()) -> companies
-datafest2018 %>% filter(companyId == "company00090") %>% mean(.$estimatedSalary)
+mean((datafest2018 %>% filter(companyId == "company00090"))$estimatedSalary)
 city_counts <- datafest2018 %>% group_by(city) %>% summarize(n()) %>% arrange(desc(`n()`)) %>% slice(2:12501)
 #write_csv(city_counts,"./data/city_counts.csv")
 industries <- datafest2018 %>% group_by(industry) %>% summarize(n()) %>% arrange(desc(`n()`))
@@ -15,3 +15,4 @@ industries <- datafest2018 %>% group_by(industry) %>% summarize(n()) %>% arrange
 #locs <- city_counts$city[n:(n+2)]
 #
 #geoLocations <- geocode(as.character(locs))
+all_cities <- datafest2018 %>% group_by(city) %>% select(city, stateProvince, country) %>% summarize(n())
